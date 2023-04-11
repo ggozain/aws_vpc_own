53655a468ea9f70075f18d0fe4c5c52baf0941c6
Gerardo Gozain <gozain@gmail.com>
2023-04-11 10:49:14 +0100

    more fixes

--- modules/nat_gw/output.tf
+++ modules/nat_gw/output.tf
@@ -1,4 +1,4 @@
 output "vpc_nat_gw_ids" {
     # Result is a list of the created NAT
-    value = tolist(aws_nat_gateway.nat_gw.*.id)
+    value = toset(aws_nat_gateway.nat_gw.*.id)
 }
--- modules/route_table_association/main.tf
+++ modules/route_table_association/main.tf
@@ -1,16 +1,16 @@
 resource "aws_route_table_association" "public" {
     for_each = var.public_subnet_ids
 
-    subnet_id = each.value
+    subnet_id = each.key
 
     route_table_id = var.public_route_table_id
   
 }
 
 resource "aws_route_table_association" "private" {
-    count = length(var.private_subnet_ids)
+    count = length(tolist(keys(var.private_subnet_ids)))
 
-    subnet_id = var.private_subnet_ids[count.index]
+    subnet_id = tolist(keys(var.private_subnet_ids))[count.index]
 
     route_table_id = var.private_route_table_ids[count.index]
   
