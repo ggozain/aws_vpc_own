ddf0d247511551718b0656a17eabb9a4a60f717e
Gerardo Gozain <gozain@gmail.com>
2023-04-13 16:28:34 +0100

    please lord x2

--- main.tf
+++ main.tf
@@ -50,7 +50,6 @@ module "routing_tables" {
 module "route_table_association" {
 
   source                  = "./modules/route_table_association"
-  vpc_id                  = module.vpc.vpc_id
   public_route_table_id   = module.routing_tables.public_route_table_id
   private_route_table_ids = module.routing_tables.private_route_table_ids
   public_subnet           = module.vpc.vpc_public_subnets
