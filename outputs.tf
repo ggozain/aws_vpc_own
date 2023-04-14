f921c3e970fa98a70a172a21e5445591a16e8641
Gerardo Gozain <gozain@gmail.com>
2023-04-14 15:08:31 +0100

    added more outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
     description = "The ID of the VPC created by the VPC module"
     value = module.vpc.vpc_id
 }
+
+output "public_subnet_id" {
+    description = "The ID of the public subnets created by the VPC module"
+    value = values(module.vpc.vpc_public_subnets)
+}
+
+output "private_subnet_id" {
+    description = "The ID of the public subnets created by the VPC module"
+    value = values(module.vpc.vpc_private_subnets)
+}
